98aa2747f400fa710898f8682b633cd908b1aaec
Calliope Kostopoulou <kelkost@yahoo.gr>
2024-04-25 07:50:05 +0000

    minimizing changes

--- tensorflow/c/experimental/saved_model/core/revived_types/flat_tensor_function.cc
+++ tensorflow/c/experimental/saved_model/core/revived_types/flat_tensor_function.cc
@@ -13,8 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include <fstream>
-
 #include "tensorflow/c/experimental/saved_model/core/revived_types/flat_tensor_function.h"
 
 #include <memory>
