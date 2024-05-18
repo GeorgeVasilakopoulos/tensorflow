8fa6cb72dafa924ee6c16306f3eeba2afb0ca314
George Vasilakopoulos <georgevas00@hotmail.com>
2024-05-18 21:58:12 +0000

    Disable control flow v2 in test file

--- recursion-tests/test2.py
+++ recursion-tests/test2.py
@@ -10,14 +10,11 @@ from tensorflow.python.framework import function
 
 
 tf.compat.v1.disable_eager_execution()
+tf.compat.v1.disable_control_flow_v2()
 
 # tf.logging.set_verbosity(tf.logging.INFO)
 fac = function.Declare("Fac", [("n", tf.int32)], [("ret", tf.int32)])
 
-
-@function.Defun(tf.int32, func_name="Test", out_names=["ret"])
-def t(n):
-	return tf.constant(1)
 
 
 
@@ -27,7 +24,7 @@ def t(n):
 def FacImpl(n):
 	return tf.cond(tf.less_equal(n, 1),
 		lambda: tf.constant(1),
-		lambda: n * fac(n - 1))
+		lambda: n * fac(n - 1)) 
 
 
 # @function.Defun(tf.int32, func_name="Fac", out_names=["ret"])
@@ -43,10 +40,10 @@ FacImpl.add_to_graph(tf.compat.v1.get_default_graph())
 print(tf.compat.v1.get_default_graph().as_graph_def())
 
 
-# writer = tf.compat.v1.summary.FileWriter('/tensorflow/TESTS/graph', tf.compat.v1.get_default_graph())
+writer = tf.compat.v1.summary.FileWriter('/tensorflow/recursion-tests/graph', tf.compat.v1.get_default_graph())
 # writer = tf.summary.FileWriter('./graphs', tf.compat.v1.get_default_graph())
 
 with tf.compat.v1.Session() as sess:
-    result = FacImpl(1)
+    result = FacImpl(10)
     print("Result:", sess.run(result))
 
